749cf51037d79c6164f58d0dc18cc66f1cc9b570
Jonathan Cady <joncady@uw.edu>
2019-01-13 01:57:27 -0800

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# joncady.github.io
-Here's my website!
---
+++ precache-manifest.77f69c81f9d7a5e6c11f55ca79c20bd5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "21812f6d8d4844da17453b80216a4445",
+    "url": "/static/media/sql.21812f6d.png"
+  },
+  {
+    "revision": "b4c68044bba02abc3d1e",
+    "url": "/static/css/main.172bde43.chunk.css"
+  },
+  {
+    "revision": "e2b8c71475a83195933b64c7981a461e",
+    "url": "/static/media/pi.e2b8c714.jpeg"
+  },
+  {
+    "revision": "359e0b4c6d9b81b52fef",
+    "url": "/static/js/1.359e0b4c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/static/media/react.580ffd26.png"
+  },
+  {
+    "revision": "718eec11056d7542e98ed1ea9b5d1f3d",
+    "url": "/static/media/otherPic.718eec11.jpg"
+  },
+  {
+    "revision": "e642c7556d83be574ae3929c34ead074",
+    "url": "/static/media/firebase.e642c755.png"
+  },
+  {
+    "revision": "b4c68044bba02abc3d1e",
+    "url": "/static/js/main.b4c68044.chunk.js"
+  },
+  {
+    "revision": "f8dab57d048fabd69ea16c67e1615b86",
+    "url": "/static/media/node.f8dab57d.png"
+  },
+  {
+    "revision": "4eb1819dc863ef386e09d86e9f482a9f",
+    "url": "/static/media/java.4eb1819d.png"
+  },
+  {
+    "revision": "ca2ceddcd9c28a22c449286f3fe66751",
+    "url": "/static/media/python.ca2ceddc.png"
+  },
+  {
+    "revision": "060a1e7ed38545d3bb83c1773b579749",
+    "url": "/static/media/r.060a1e7e.png"
+  },
+  {
+    "revision": "fb324fa2e38c27f1db43c7f86f1ee7af",
+    "url": "/static/media/bs.fb324fa2.png"
+  },
+  {
+    "revision": "2f282e929ffd751264f6fcc817ec7332",
+    "url": "/static/media/api.2f282e92.png"
+  },
+  {
+    "revision": "359e0b4c6d9b81b52fef",
+    "url": "/static/css/1.1bd3236d.chunk.css"
+  },
+  {
+    "revision": "eff3caf05ce884b56313866d6d02d547",
+    "url": "/index.html"
+  }
+];
